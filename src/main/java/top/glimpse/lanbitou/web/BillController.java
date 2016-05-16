69a4c05757e0163c0eb5496495a8a982491cc785
Henvealf <1103693087@qq.com>
2016-05-16 19:44:18 +0800

    编写了 添加新bill记录的控制器方法addOne.

--- src/main/java/top/glimpse/lanbitou/web/BillController.java
+++ src/main/java/top/glimpse/lanbitou/web/BillController.java
@@ -2,8 +2,14 @@ package top.glimpse.lanbitou.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import top.glimpse.lanbitou.data.BillRepository;
+import top.glimpse.lanbitou.domain.Bill;
+
+import java.util.Date;
 
 /**
  * Created by Henvealf on 16-5-14.
@@ -18,6 +24,22 @@ public class BillController {
     @Autowired
     public BillController(BillRepository billRepository){
         this.billRepository = billRepository;
+    }
+
+    @RequestMapping(value="/addOne" ,method = RequestMethod.POST )
+    public void addOne(Bill bill,Model model){
+
+        bill = new Bill();          //先试一下
+        bill.setUid(1);
+        bill.setType("玩");
+        bill.setMoney(-34.5);
+        bill.setFolder("日常");
+        bill.setRemark("去了阿尔卑斯玩一玩");
+
+        if(bill.getBillDate() == null){
+            bill.setBillDate(new Date());
+        }
+        billRepository.addOne(bill);
     }
 
 }
