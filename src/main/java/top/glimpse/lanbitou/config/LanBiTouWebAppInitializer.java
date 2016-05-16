a44fc0b12dcc60116bbae8e3936afe4d6aaca014
Henvealf <1103693087@qq.com>
2016-05-16 19:45:14 +0800

    增加了一个编码过滤器.不知管不管用.

--- src/main/java/top/glimpse/lanbitou/config/LanBiTouWebAppInitializer.java
+++ src/main/java/top/glimpse/lanbitou/config/LanBiTouWebAppInitializer.java
@@ -1,6 +1,9 @@
 package top.glimpse.lanbitou.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 /**
  * Created by joyce on 16-5-11.
@@ -14,6 +17,15 @@ public class LanBiTouWebAppInitializer extends AbstractAnnotationConfigDispatche
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[] {WebConfig.class};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("utf-8");
+        encodingFilter.setForceEncoding(true);
+
+        return new Filter[]{encodingFilter};
     }
 
     @Override
