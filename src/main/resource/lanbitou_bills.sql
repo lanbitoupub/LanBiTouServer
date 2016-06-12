096e3151ba8d6d101fe3af0635370a11fc5abdc6
Henvealf <1103693087@qq.com>
2016-06-12 11:38:54 +0800

    新的Bill表,或许没改动.然后吧billFolder表删除了就可以.

--- src/main/resource/lanbitou_bills.sql
+++ src/main/resource/lanbitou_bills.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `lanbitou` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `lanbitou`;
 -- MySQL dump 10.13  Distrib 5.7.9, for linux-glibc2.5 (x86_64)
 --
 -- Host: localhost    Database: lanbitou
@@ -27,13 +29,14 @@ CREATE TABLE `bills` (
   `uid` int(11) DEFAULT NULL,
   `type` varchar(10) NOT NULL,
   `money` double NOT NULL,
-  `folder` varchar(50) NOT NULL,
+  `folder` varchar(20) NOT NULL,
   `remark` varchar(300) NOT NULL,
   `bill_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `uid` (`uid`),
+  KEY `fk_bills_1_idx` (`folder`,`uid`),
+  KEY `bills_ibfk_1_idx` (`uid`),
   CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=174 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +45,7 @@ CREATE TABLE `bills` (
 
 LOCK TABLES `bills` WRITE;
 /*!40000 ALTER TABLE `bills` DISABLE KEYS */;
-INSERT INTO `bills` VALUES (1,1,'喝',-45.6,'日常','哈哈哈,喝的也不错','2016-05-16 09:57:42'),(2,1,'吃',-50.5,'日常','在XXX吃的很饱,下次再来','2016-05-16 09:59:00'),(3,1,'发工资喽',5200,'日常','好开心','2016-05-16 10:01:30'),(11,1,'玩',-34.5,'日常','去了阿尔卑斯玩一玩','2016-05-16 11:32:10');
+INSERT INTO `bills` VALUES (116,1,'约定',1234,'你说什么？吼吼','是一首歌。哈哈','2016-05-25 16:00:00'),(118,1,'库',68,'你说什么？吼吼','理解','2016-05-25 16:00:00'),(123,1,'有位朋友',-52,'你说什么？吼吼','还记得吗！','2016-05-25 16:00:00'),(124,1,'哈喽',52,'你说什么？吼吼','啦啦','2016-05-25 16:00:00'),(147,1,'0',0,'吉他的和弦2','0','2016-06-04 13:08:58'),(150,1,'0',0,'勿忘添加','0','2016-06-04 13:19:28'),(151,1,'呦吼',154,'你说什么？吼吼','啦啦','2016-06-06 16:00:00'),(152,1,'吃',455,'吉他的和弦2','吃了','2016-06-07 16:00:00'),(154,1,'买',-54,'吉他的和弦2','一个吉他。','2016-06-07 16:00:00'),(155,1,'买衣服',-565,'你说什么？吼吼','是个裤子','2016-06-07 16:00:00'),(156,1,'书',-23,'你说什么？吼吼','没有人给他写信的上校','2016-06-07 16:00:00'),(157,1,'哈哈',86,'你说什么？吼吼','偷','2016-06-01 16:00:00'),(158,1,'12',-85,'你说什么？吼吼','她','2016-06-07 16:00:00'),(159,1,'11',-11,'你说什么？吼吼','11','2016-06-07 16:00:00'),(162,1,'啦',55,'勿忘添加','看了看','2016-06-07 16:00:00'),(163,1,'你就',26,'勿忘添加','看来','2016-06-07 16:00:00'),(164,1,'哈哈',56,'吉他的和弦2','啦啦','2016-06-07 16:00:00'),(166,1,'我看看',55,'你说什么？吼吼','来','2016-06-01 16:00:00'),(167,1,'看来',85,'你说什么？吼吼','可怜','2016-06-07 16:00:00'),(168,1,'那就',55,'你说什么？吼吼','咯','2016-06-07 16:00:00'),(170,1,'？？',936,'你说什么？吼吼','头','2016-06-07 16:00:00'),(171,1,'哈哈',96,'勿忘添加','看来','2016-06-07 16:00:00'),(172,1,'在头',65,'勿忘添加','看来','2016-06-07 16:00:00'),(173,1,'吃法',55,'勿忘添加','可怜','2016-06-07 16:00:00');
 /*!40000 ALTER TABLE `bills` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,9 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
-select * from bills;
-select * from note;
-
-select * from user;
--- Dump completed on 2016-05-16 19:38:21
+-- Dump completed on 2016-06-12 11:37:16
